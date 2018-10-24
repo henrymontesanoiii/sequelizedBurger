1da93143742119378fd62e9c27a343dccccc5327
henman3 <henrymontesanoiii@gmail.com>
2018-10-24 16:36:48 -0400

    First commit

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (name, devoured) VALUES ('Quarter Pounder', false);
+INSERT INTO burgers (name, devoured) VALUES ('Baconator', true);
+INSERT INTO burgers (name, devoured) VALUES ('Big Mac', false);
+
